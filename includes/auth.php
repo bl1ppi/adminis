00cf9e1223724aa9573c22e53b5aa62d538f7e16
bl1ppi <bvd.027@gmail.com>
2025-06-23 10:15:05 +0700

    fix auth.php

--- includes/auth.php
+++ includes/auth.php
@@ -1,4 +1,6 @@
 <?php
+ini_set('session.gc_maxlifetime', 43200);
+session_set_cookie_params(43200);
 session_start();
 if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
     header("Location: /adminis/login.php");
